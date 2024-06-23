eb38dee1689adce18c1a065af651f90f7742c422
Ivan Facchinetti <ivan.facchinetti55@gmail.com>
2024-06-23 16:03:29 +0200

    Lightbox and News fix

--- js/unsereProdukte.js
+++ js/unsereProdukte.js
@@ -6,6 +6,6 @@ document.querySelectorAll('.img-container img').forEach(image => {
     });
 });
 
-document.querySelector('.close').addEventListener('click', function() {
+document.querySelector('.lightbox .close').addEventListener('click', function() {
     document.querySelector('.lightbox').style.display = 'none';
 });
